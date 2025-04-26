751c0757e9e56fa7a5214130e0e77e9aa4f9ba62
ChristineDomat-ol <christinedomatol@gmail.com>
2025-04-26 13:10:01 +0800

    Add project files.

---
+++ TaskManager/Program.cs
@@ -0,0 +1,55 @@
+﻿using TODOService;
+
+namespace TaskManager
+{
+    internal class Program
+    {
+        static TaskService taskService = new TaskService();
+
+        static string[] Actions = new string[] {
+            "[1] Display All Task",
+            "[2] Display All Task In Progress," +
+            "[3] Create a NEW task",
+            "[4] Update a task",
+            "[5] Delete a task"
+        };
+        static void Main(string[] args)
+        {
+            DisplayActions();
+            string action = GetUserInput();
+
+            switch (action)
+            {
+                case "1":
+                    DisplayAllTask();
+                    break;
+            }
+
+        }
+
+        public static void DisplayActions()
+        {
+            Console.WriteLine("ACTIONS: ");
+            foreach (var action in Actions)
+            {
+                Console.WriteLine(action);
+            }
+        }
+
+        public static string GetUserInput()
+        {
+            Console.Write("\nAction: ");
+            string action = Console.ReadLine();
+            return action;
+        }
+
+        public static void DisplayAllTask()
+        {
+            foreach (var task in taskService.GetAllTasks())
+            {
+                Console.WriteLine(task);
+            }
+        }
+    }
+}
+
---
+++ TaskManager/TODOCommon/Task.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Enum;
+
+namespace TODOCommon
+{
+    public class Task
+    {
+        private string _status;
+        public int TaskId { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string Status
+        {
+            get { return _status; }
+            set
+            {
+                if (value.Equals("Not_Started") || value.Equals("In_Progress") || value.Equals("Done"))
+                {
+                    _status = value;
+                }
+            }
+        }
+    }
+}
